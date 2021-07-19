7614aba517882aeac439c53fda7f644987b0a2b3
binh.buidang <dangbinh4869@gmail.com>
2021-07-19 10:42:40 +0700

    Updates

---
+++ precache-manifest.2cf047a11b5e14321d0430b8da4ebbf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eaf41d736222c2754b02e385b1897b6",
+    "url": "/dunkbing/index.html"
+  },
+  {
+    "revision": "d7a43e75d67e5219a10a",
+    "url": "/dunkbing/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "a17bcf52e5f8516c639f",
+    "url": "/dunkbing/static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "d7a43e75d67e5219a10a",
+    "url": "/dunkbing/static/js/2.01d31089.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/dunkbing/static/js/2.01d31089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a17bcf52e5f8516c639f",
+    "url": "/dunkbing/static/js/main.d53047a1.chunk.js"
+  },
+  {
+    "revision": "503cd104525866047d8d",
+    "url": "/dunkbing/static/js/runtime-main.88e71b95.js"
+  }
+]);
